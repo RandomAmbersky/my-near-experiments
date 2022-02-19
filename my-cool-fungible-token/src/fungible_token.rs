bc5277369fcaf5a5506e6ee8ad06996f85af5b1c
sir Random <RandomAmbersky@gmail.com>
2022-02-19 19:33:58 +0300

    update

--- my-cool-fungible-token/src/fungible_token.rs
+++ my-cool-fungible-token/src/fungible_token.rs
@@ -6,3 +6,13 @@ use near_sdk::{ PanicOnDefault, near_bindgen};
 pub struct Contract {
 	pub num_accounts: u32
 }
+
+#[near_bindgen]
+impl Contract {
+	#[init]
+	pub fn new() -> Self {
+		Self {
+			num_accounts: 0
+		}
+	}
+}
