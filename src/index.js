8a9cae6530d1bc68917864d969210279286f902b
sir Random <RandomAmbersky@gmail.com>
2022-03-25 19:02:15 +0300

    some upd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import { initContract } from './utils'
 
-window.nearInitPromise = initContract()
-  .then(() => {
-    ReactDOM.render(
-      <App />,
-      document.querySelector('#root')
-    )
-  })
-  .catch(console.error)
+import App from './App'
+
+ReactDOM.render( <App />, document.querySelector('#root'))
