8777fd68ed5bb5af410a64e9e1913c542d66755c
sir Random <RandomAmbersky@gmail.com>
2022-05-02 11:28:29 +0300

    some update

--- my-ft/src/lib.rs
+++ my-ft/src/lib.rs
@@ -3,11 +3,14 @@ mod ft;
 use near_contract_standards::fungible_token::FungibleToken;
 use near_contract_standards::fungible_token::metadata::{FT_METADATA_SPEC, FungibleTokenMetadata, FungibleTokenMetadataProvider};
 
-use near_sdk::{env, near_bindgen};
+use near_sdk::{Balance, env, log, near_bindgen};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{PanicOnDefault};
 use near_sdk::{AccountId, PromiseOrValue};
 use near_sdk::json_types::{U128};
+
+const FT_METADATA_NAME: &str = "Eternal Lands Gold Token";
+const FT_METADATA_SYMBOL: &str = "elGOLD";
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -24,25 +27,34 @@ impl MyPrettyFungibleToken {
 			ft: FungibleToken::new(b"a".to_vec()),
 		};
 		this.ft.internal_register_account(&account_id);
-		this.ft.internal_deposit(&account_id, 0);
+		this.ft.internal_deposit(&account_id, 5000);
 		this
 	}
+
+	fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
+		log!("Closed @{} with {}", account_id, balance);
+	}
+
+	fn on_tokens_burned(&mut self, account_id: AccountId, amount: Balance) {
+		log!("Account @{} burned {}", account_id, amount);
+	}
+
 }
 
-near_contract_standards::impl_fungible_token_core!(MyPrettyFungibleToken, ft);
-near_contract_standards::impl_fungible_token_storage!(MyPrettyFungibleToken, ft);
+near_contract_standards::impl_fungible_token_core!(MyPrettyFungibleToken, ft, on_tokens_burned);
+near_contract_standards::impl_fungible_token_storage!(MyPrettyFungibleToken, ft, on_account_closed);
 
 #[near_bindgen]
 impl FungibleTokenMetadataProvider for MyPrettyFungibleToken {
 	fn ft_metadata(&self) -> FungibleTokenMetadata {
 		FungibleTokenMetadata {
-			spec: FT_METADATA_SPEC.to_string(),
-			name: "My Cool Pretty Token".into(),
-			symbol: "GOLDO".into(),
+			spec: FT_METADATA_SPEC.to_owned(),
+			name: FT_METADATA_NAME.to_owned(),
+			symbol: FT_METADATA_SYMBOL.to_owned(),
 			icon: None,
 			reference: None,
 			reference_hash: None,
-			decimals: 6,
+			decimals: 1,
 		}
 	}
 }
