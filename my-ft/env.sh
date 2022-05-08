3db7efb6e40bd9038bfdbfc8420f1c18229a9971
sir Random <RandomAmbersky@gmail.com>
2022-05-08 16:04:54 +0300

    prepare to balance pool

--- my-ft/env.sh
+++
@@ -1,4 +0,0 @@
-#bin/bash
-
-source ./neardev/dev-account.env
-echo "CONTRACT_NAME=${CONTRACT_NAME}"
--- my-ft/pipeline.sh
+++ my-ft/pipeline.sh
@@ -1,5 +1,9 @@
 NEAR=$(yarn bin near)
 
+function do_import_env() {
+    source ./neardev/dev-account.env
+}
+
 function build () {
   cargo build --manifest-path ./Cargo.toml --target wasm32-unknown-unknown --release
 }
@@ -9,67 +13,61 @@ function dev_deploy() {
 }
 
 function dev_new() {
-  source ../neardev/dev-account.env
   echo "$CONTRACT_NAME"
-  yarn near call "$CONTRACT_NAME" new --accountId "$CONTRACT_NAME"
+  $NEAR call "$CONTRACT_NAME" new --accountId "$CONTRACT_NAME"
 }
 
 function dev_view () {
-    source ../neardev/dev-account.env
     yarn near view "$CONTRACT_NAME" ft_metadata
 }
 
 function dev_storage_deposit () {
   ID=$1
   echo "$ID"
-  source ../neardev/dev-account.env
-  yarn near call "$CONTRACT_NAME" storage_deposit '' --accountId "$ID" --amount 0.00125
+  do_import_env
+  $NEAR call "$CONTRACT_NAME" storage_deposit '' --accountId "$ID" --amount 0.00125
 }
 
 function dev_balance () {
   ID=$1
   echo "$ID"
-  source ../neardev/dev-account.env
-  yarn near view "$CONTRACT_NAME" ft_balance_of '{"account_id": "'"$ID"'"}'
+  $NEAR view "$CONTRACT_NAME" ft_balance_of '{"account_id": "'"$ID"'"}'
 }
 
 function dev_transfer () {
   ID=$1
   AMOUNT=$2
   echo "$ID -> $AMOUNT"
-  source ../neardev/dev-account.env
-  yarn near call "$CONTRACT_NAME" ft_transfer '{"receiver_id": "'"$ID"'", "amount": "'"$AMOUNT"'"}' --accountId "$CONTRACT_NAME" --amount 0.000000000000000000000001
+  $NEAR call "$CONTRACT_NAME" ft_transfer '{"receiver_id": "'"$ID"'", "amount": "'"$AMOUNT"'"}' --accountId "$CONTRACT_NAME" --amount 0.000000000000000000000001
 }
 
 function dev_mint () {
   ID=$1
   AMOUNT=$2
   echo "$ID -> $AMOUNT"
-  source ../neardev/dev-account.env
-  yarn near call "$CONTRACT_NAME" ft_mint '{"receiver_id": "'"$ID"'", "amount": "'"$AMOUNT"'"}' --accountId "$CONTRACT_NAME" --amount 0.000000000000000000000001
+  $NEAR call "$CONTRACT_NAME" ft_mint '{"receiver_id": "'"$ID"'", "amount": "'"$AMOUNT"'"}' --accountId "$CONTRACT_NAME" --amount 0.000000000000000000000001
 }
 
 function dev_delete () {
   BENEFICIARY_ID=$1
-  source ./neardev/dev-account.env
   $NEAR delete "$CONTRACT_NAME" "$BENEFICIARY_ID"
 }
 
-function get_owner() {
-  source ../neardev/dev-account.env
-  yarn near view "$CONTRACT_NAME" get_owner
-}
+#function get_owner() {
+#  do_import_env
+#  $NEAR view "$CONTRACT_NAME" get_owner
+#}
 
 function empty() {
   echo "command not found: ${1} ${2} ${3}"
 }
 
 function main() {
-
+  do_import_env
   if [ "$1" == "build" ]; then
     build
-  elif [ "$1" == "get-owner" ]; then
-    get_owner
+#  elif [ "$1" == "get-owner" ]; then
+#    get_owner
   elif [ "$1" == "dev-deploy" ]; then
     dev_deploy
   elif [ "$1" == "dev-new" ]; then
