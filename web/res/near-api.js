88f043d9d037d36ddbb919510bfafee543a296a9
sir Random <RandomAmbersky@gmail.com>
2022-05-21 22:21:07 +0300

    some refactoring

--- web/res/near-api.js
+++ web/res/near-api.js
@@ -10,32 +10,30 @@ const TestNearConfig = {
 	walletUrl: "https://wallet.testnet.near.org",
 	helperUrl: 'https://helper.testnet.near.org',
 	contractPoolName: 'ref-finance-101.testnet'
-};
+}
 const MainNearConfig = {
 	networkId: "mainnet",
 	nodeUrl: "https://rpc.mainnet.near.org",
 	walletUrl: "https://wallet.near.org",
 	helperUrl: 'https://helper.mainnet.near.org',
 	contractPoolName: 'ref-finance-101.testnet'
-};
-
-const config = {
-	appName: IsMainnet ? "elGOLD bank" : "elGOLD bank",
-	nearConfig: IsMainnet ? MainNearConfig : TestNearConfig
 }
+
+const nearConfig = IsMainnet ? MainNearConfig : TestNearConfig
 
 async function init () {
 	const near = await nearApiJS.connect(
-		Object.assign(config.nearConfig, {
+		Object.assign(nearConfig, {
 			deps: {
 				keyStore: new nearApiJS.keyStores.BrowserLocalStorageKeyStore()
 			}
 		}))
 	const walletAccount = new nearApiJS.WalletConnection(near)
 	const accountId = walletAccount.getAccountId();
+
 	const poolContract = await poolAPI.initContract({
 		nearApiJS,
-		contractName: config.nearConfig.contractPoolName,
+		contractName: nearConfig.contractPoolName,
 		walletAccount
 	})
 
