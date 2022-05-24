9a346c434cb286f34c8fce7a56b30a6689115e1b
sir Random <RandomAmbersky@gmail.com>
2022-05-24 13:15:54 +0300

    some strange

--- web/res/App.js
+++ web/res/App.js
@@ -1,7 +1,10 @@
 import React from "react"
-import {useNearInit} from "./hooks/useNearApi";
-
+import {useNearInit} from "./hooks/useNearApi"
 import PoolInfo from './components/Poolnfo'
+
+import { Box, ThemeProvider } from '@mui/system'
+import {createTheme} from '@mui/material/styles'
+import {CssBaseline} from '@mui/material'
 
 const poolId = 474
 
@@ -12,16 +15,37 @@ const initialCtxState = {
 	near: null
 }
 
+const theme = createTheme({
+	palette: {
+		background: {
+			paper: '#fff',
+		},
+		text: {
+			primary: '#173A5E',
+			secondary: '#46505A',
+		},
+		action: {
+			active: '#001E3C',
+		},
+		success: {
+			dark: '#009688',
+		},
+	},
+});
+
 export function App () {
 	console.log("App...")
 	const ctx = useNearInit(initialCtxState)
 
 	return (
-		<div>
-			<h1>React is here</h1>
-      {
-        ctx.poolContract ? <PoolInfo poolContract={ctx.poolContract} poolId={poolId}/>  : <h1>is loading...</h1>
-      }
-		</div>
+		<ThemeProvider theme={theme}>
+			<Box>
+				<CssBaseline />
+				<h1>React is here</h1>
+				{
+					ctx.poolContract ? <PoolInfo poolContract={ctx.poolContract} poolId={poolId}/>  : <h1>is loading...</h1>
+				}
+			</Box>
+		</ThemeProvider>
 	)
 }
